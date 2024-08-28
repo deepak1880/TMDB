4343697276967da95442220e50503215398676ec
abdulgani <deepak.mandal@neosoftmail.com>
2024-08-28 13:36:30 +0530

    shimmer effect to search

--- app/src/main/java/com/shaikhabdulgani/tmdb/search/presentation/SearchState.kt
+++ app/src/main/java/com/shaikhabdulgani/tmdb/search/presentation/SearchState.kt
@@ -2,6 +2,5 @@ package com.shaikhabdulgani.tmdb.search.presentation
 
 data class SearchState<out T>(
     val list: List<T> = emptyList(),
-    val isLoading: Boolean = false,
     val endReached: Boolean = false,
 )
